5eb66a5fb2b87003b8393afa526d578ea8909377
Joe Sweeney <joe@j1.io>
2017-09-24 17:05:25 -0400

    Rename seeds.ex, improve controller test

--- test/controllers/system_controller_test.exs
+++ test/controllers/system_controller_test.exs
@@ -2,7 +2,52 @@ defmodule Hpd.SystemControllerTest do
   use Hpd.ConnCase
 
   alias Hpd.System
-  @valid_attrs %{nodes_cpuAvgMax: 42, osVersion: "some osVersion", disksState: "some disksState", performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis: 120.5, capacity_byType_fc_freeTiB: 120.5, performance_summary_portInfo_totalServiceTimeMillis: 120.5, serialNumber: 42, capacity_total_sizeTiB: 120.5, performance_portBandwidthData_total_iopsAvg: 42, performance_summary_portInfo_readServiceTimeMillis: 120.5, performance_portBandwidthData_total_dataRateKBPSAvg: 120.5, disks_total_diskCount: 42, performance_summary_vvInfo_vvsByType_ssd_readBandwidthMBPS: 120.5, capacity_total_freePct: 120.5, capacity_total_compressionRatio: 120.5, nodes_nodeCountOffline: 42, capacity_total_freeTiB: 120.5, capacity_byType_ssd_freeTiB: 120.5, performance_summary_delAckPct: 120.5, disks_total_diskCountNormal: 42, vvCount: 42, capacity_byType_ssd_sizeTiB: 120.5, companyName: "some companyName", performance_portBandwidthData_total_iopsMax: 120.5, cpgCount: 42, recommended_osVersion: "some recommended_osVersion", nodes_nodeCount: 42, location_country: "some location_country", capacity_byType_fc_sizeTiB: 120.5, disks_total_diskCountFailed: 42, systemName: "some systemName", capacity_byType_nl_sizeTiB: 120.5, installDate: ~N[2010-04-17 14:00:00.000000], performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS: 120.5, productFamily: 42, capacity_total_dedupeRatio: 120.5, capacity_byType_nl_freeTiB: 120.5, performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis: 120.5, updated: ~N[2010-04-17 14:00:00.000000], disks_total_diskCountDegraded: 42, performance_summary_portInfo_writeServiceTimeMillis: 120.5, capacity_total_compactionRatio: 120.5, tpvvCount: 42, location_region: "some location_region", model: "some model"}
+  @valid_attrs %{
+    "nodes_cpuAvgMax" => 42, 
+    "osVersion" => "some osVersion", 
+    "disksState" => "some disksState", 
+    "performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis" => 120.5, 
+    "capacity_byType_fc_freeTiB" => 120.5, 
+    "performance_summary_portInfo_totalServiceTimeMillis" => 120.5, 
+    "serialNumber" => 42, 
+    "capacity_total_sizeTiB" => 120.5, 
+    "performance_portBandwidthData_total_iopsAvg" => 42, 
+    "performance_summary_portInfo_readServiceTimeMillis" => 120.5, 
+    "performance_portBandwidthData_total_dataRateKBPSAvg" => 120.5, 
+    "disks_total_diskCount" => 42, 
+    "performance_summary_vvInfo_vvsByType_ssd_readBandwidthMBPS" => 120.5, 
+    "capacity_total_freePct" => 120.5, 
+    "capacity_total_compressionRatio" => 120.5, 
+    "nodes_nodeCountOffline" => 42, 
+    "capacity_total_freeTiB" => 120.5, 
+    "capacity_byType_ssd_freeTiB" => 120.5, 
+    "performance_summary_delAckPct" => 120.5, 
+    "disks_total_diskCountNormal" => 42, 
+    "vvCount" => 42, 
+    "capacity_byType_ssd_sizeTiB" => 120.5, 
+    "companyName" => "some companyName", 
+    "performance_portBandwidthData_total_iopsMax" => 120.5, 
+    "cpgCount" => 42, 
+    "recommended_osVersion" => "some recommended_osVersion", 
+    "nodes_nodeCount" => 42, 
+    "location_country" => "some location_country", 
+    "capacity_byType_fc_sizeTiB" => 120.5, 
+    "disks_total_diskCountFailed" => 42, 
+    "systemName" => "some systemName", 
+    "capacity_byType_nl_sizeTiB" => 120.5, 
+    "installDate" => ~N[2010-04-17 14:00:00.000000], 
+    "performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS" => 120.5, 
+    "productFamily" => 42, 
+    "capacity_total_dedupeRatio" => 120.5, 
+    "capacity_byType_nl_freeTiB" => 120.5, 
+    "performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis" => 120.5, 
+    "updated" => ~N[2010-04-17 14:00:00.000000],
+    "disks_total_diskCountDegraded" => 42, 
+    "performance_summary_portInfo_writeServiceTimeMillis" => 120.5, 
+    "capacity_total_compactionRatio" => 120.5, 
+    "tpvvCount" => 42, 
+    "location_region" => "some location_region", 
+    "model" => "some model"}
   @invalid_attrs %{}
 
   setup %{conn: conn} do
@@ -15,9 +60,13 @@ defmodule Hpd.SystemControllerTest do
   end
 
   test "shows chosen resource", %{conn: conn} do
-    system = Repo.insert! %System{}
+    system =
+      %System{}
+      |> System.changeset(@valid_attrs)
+      |> Repo.insert!()
     conn = get conn, system_path(conn, :show, system)
-    assert json_response(conn, 200)["data"] == %{"id" => system.id,
+    assert json_response(conn, 200)["data"] == %{
+      "id" => system.id,
       "companyName" => system.companyName,
       "systemName" => system.systemName,
       "serialNumber" => system.serialNumber,
@@ -28,8 +77,8 @@ defmodule Hpd.SystemControllerTest do
       "recommended_osVersion" => system.recommended_osVersion,
       "location_region" => system.location_region,
       "location_country" => system.location_country,
-      "installDate" => system.installDate,
-      "updated" => system.updated,
+      "installDate" => NaiveDateTime.to_iso8601(system.installDate),
+      "updated" => NaiveDateTime.to_iso8601(system.updated),
       "nodes_nodeCount" => system.nodes_nodeCount,
       "nodes_nodeCountOffline" => system.nodes_nodeCountOffline,
       "disks_total_diskCount" => system.disks_total_diskCount,
@@ -62,7 +111,7 @@ defmodule Hpd.SystemControllerTest do
       "performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS" => system.performance_summary_vvInfo_vvsByType_ssd_writeBandwidthMBPS,
       "performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis" => system.performance_summary_vvInfo_vvsByType_ssd_readServiceTimeMillis,
       "performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis" => system.performance_summary_vvInfo_vvsByType_ssd_writeServiceTimeMillis,
-      "nodes_cpuAvgMax" => system.nodes_cpuAvgMax}
+      "nodes_cpuAvgMax" => system.nodes_cpuAvgMax} 
   end
 
   test "renders page not found when id is nonexistent", %{conn: conn} do
